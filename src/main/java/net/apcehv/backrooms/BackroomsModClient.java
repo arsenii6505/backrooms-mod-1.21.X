ad37066fff36fe16c5f4561292bbe60e5694ffc9
ApceHv <apcehuu007@gmail.com>
2025-08-28 00:09:02 +0500

    Initial commit

---
+++ src/main/java/net/apcehv/backrooms/BackroomsModClient.java
@@ -0,0 +1,12 @@
+package net.apcehv.backrooms;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BackroomsModClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient(){
+
+    }
+
+}
---
+++ src/main/java/net/apcehv/backrooms/BackroomsModDataGenerator.java
@@ -0,0 +1,15 @@
+package net.apcehv.backrooms;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.loader.impl.util.log.Log;
+
+import java.io.Console;
+import java.util.logging.Logger;
+
+public class BackroomsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        int a = 0;
+	}
+}
